574eb3ab7dba62c3be5627f66149d136e1a2a277
leonardopadilha <leonardopadilha28@gmail.com>
2023-03-14 11:18:12 -0300

    Adicionando GET por cargo nos testes

--- test/cargo.spec.js
+++ test/cargo.spec.js
@@ -57,6 +57,20 @@ describe('Cargos', () => {
         });
     });
 
+    context('Que informo o id do cargo salvo', () => {
+        it('Então o cargo deve ser retornado com sucesso', (done) => {
+            request
+                .get(`${endpoints.GET_CARGO}/${id}`)
+                .end((err, res) => {
+                    expect(res).to.have.status(200)
+                    expect(res.body.id).to.equals(id)
+                    expect(res.body.nome).to.equals(data.sucess.nome)
+                    expect(res.body.descricao).to.equals(data.sucess.descricao)
+                    done()
+                })
+        })
+    })
+
     context('Dado que informo o id de um cargo para exclusão', () => {
         it('Então o cargo deve ser excluído com sucesso', (done) => {
             request
--- test/constants/constants.js
+++ test/constants/constants.js
@@ -1,7 +1,9 @@
 const POST_CARGOS='/cargos';
 const DELETE_CARGO='/cargos/cargo';
+const GET_CARGO='/cargos';
 
 module.exports = {
     POST_CARGOS,
-    DELETE_CARGO
+    DELETE_CARGO,
+    GET_CARGO
 }
