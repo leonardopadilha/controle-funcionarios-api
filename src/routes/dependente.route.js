0c522bd73b722e3f87c0072f8b136bbf0ccf28a8
leonardopadilha <leonardopadilha28@gmail.com>
2023-03-21 06:30:01 -0300

    Pesquisar por query - Route

--- src/routes/dependente.route.js
+++ src/routes/dependente.route.js
@@ -5,6 +5,7 @@ const dependenteController = require('../controllers/dependente.controller');
 const dependenteValidator = require('../validators/dependente.validator')
 
 router
+    .get('/buscar', dependenteController.pesquisarPorQuery)
     .get('/', dependenteController.pesquisarTodosDependentes)
     .get('/:id', dependenteValidator.pesquisarParentescoPorId(), dependenteController.pesquisarPorId)
     .post('/', dependenteValidator.criar(), dependenteController.criar)
