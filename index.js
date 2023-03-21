9c65c0ab446f9f8dc4e48abd9cf9d656c32ace4f
leonardopadilha <leonardopadilha28@gmail.com>
2023-03-21 05:30:29 -0300

    Adicionando dependentes em index

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ const app = express();
 
 const funcionarioRoute = require('./src/routes/funcionario.route');
 const cargoRoute = require('./src/routes/cargo.route');
-const departamentoRoute = require('./src/routes/departamento.route')
+const departamentoRoute = require('./src/routes/departamento.route');
+const dependenteRoute = require('./src/routes/dependente.route');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api/funcionarios/', funcionarioRoute);
-app.use('/api/cargos', cargoRoute)
-app.use('/api/departamentos', departamentoRoute)
+app.use('/api/cargos', cargoRoute);
+app.use('/api/departamentos', departamentoRoute);
+app.use('/api/dependentes', dependenteRoute);
 
 app.use(handle_404Error);
 app.use(handleError)
