ffcada1625017d025e85cd7689a31196a84e881c
leonardopadilha <leonardopadilha28@gmail.com>
2023-03-24 07:15:48 -0300

    Atualizando index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const funcionarioRoute = require('./src/routes/funcionario.route');
 const cargoRoute = require('./src/routes/cargo.route');
 const departamentoRoute = require('./src/routes/departamento.route');
 const dependenteRoute = require('./src/routes/dependente.route');
+const projetoRoute = require('./src/routes/projeto.route')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -20,6 +21,7 @@ app.use('/api/funcionarios/', funcionarioRoute);
 app.use('/api/cargos', cargoRoute);
 app.use('/api/departamentos', departamentoRoute);
 app.use('/api/dependentes', dependenteRoute);
+app.use('/api/projetos', projetoRoute);
 
 app.use(handle_404Error);
 app.use(handleError)
