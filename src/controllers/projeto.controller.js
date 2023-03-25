6f0dcb444dff55725636682f48b2831c00a36560
leonardopadilha <leonardopadilha28@gmail.com>
2023-03-25 05:42:05 -0300

    Alterando projeto.controller

--- src/controllers/projeto.controller.js
+++ src/controllers/projeto.controller.js
@@ -17,6 +17,64 @@ const criar = async function(req, res, next) {
     }
 }
 
+const criarVariosProjetos = async function(req, res, next) {
+    try {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty() ) {
+            throw createError(422, { errors: errors.array() })
+        }
+
+        const response = await projetoService.criarVariosProjetos(req.body)
+        res.send(response)
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const pesquisarTodosProjetos = async function(req, res, next) {
+    try {
+        const response = await projetoService.pesquisarTodosProjetos();
+
+        if (response && response.message) {
+            throw response;
+        }
+
+        res.send(response)
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const pesquisarProjetoPorId = async function(req, res, next) {
+    try {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            throw createError(422, { errors: errors.array()})
+        }
+
+        const response = await projetoService.pesquisarProjetoPorId(req.params.id)
+
+        if (response && response.message) {
+            throw response;
+        }
+
+        res.send(response)
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const pesquisarProjetoPorQuery = async function(req, res, next) {
+    try {
+        const response = await projetoService.pesquisarProjetoPorQuery(req.query)
+        res.send(response)
+    } catch (error) {
+        return next(error)
+    }
+}
+
 const deletarProjetos = async function(req, res, next) {
     try {
         const response = await projetoService.deletarProjeto(req.params.id);
@@ -33,5 +91,9 @@ const deletarProjetos = async function(req, res, next) {
 
 module.exports = {
     criar: criar,
+    criarVariosProjetos: criarVariosProjetos,
+    pesquisarTodosProjetos: pesquisarTodosProjetos,
+    pesquisarProjetoPorId: pesquisarProjetoPorId,
+    pesquisarProjetoPorQuery: pesquisarProjetoPorQuery,
     deletarProjetos: deletarProjetos
 }
