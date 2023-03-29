2c3b719c239fdb582780a4838d9e089ee3933c84
leonardopadilha <leonardopadilha28@gmail.com>
2023-03-29 12:53:20 -0300

    Adicionando trabalha.controller

---
+++ src/controllers/trabalha.controller.js
@@ -0,0 +1,50 @@
+const trabalhaService = require('../services/trabalha.service');
+const { validationResult } = require('express-validator');
+const createError = require('http-errors');
+
+const criar = async function(req, res, next) {
+    try {
+        const errors = validationResult(req)
+
+        if (errors.isEmpty()) {
+            throw createError(422, { errors: errors.array() })
+        }
+
+        const response = await trabalhaService.criar(req.body)
+        res.send(response)
+
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const criarVariosRegistros = async function(req, res, next) {
+    try {
+        const errors = validationResult(req)
+
+        if (errors.isEmpty()) {
+            throw createError(422, { errors: errors.array() })
+        }
+
+        const response = await trabalhaService.criarVariosRegistros(req.body)
+        res.send(response)
+
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const pesquisarDadosTrabalhador = async function(req, res, next) {
+    try {
+        const response = await trabalhaService.pesquisarDadosTrabalhador()
+
+        if (response && response.message) {
+            throw response;
+        }
+        
+        res.send(response)
+
+    } catch (error) {
+        return next(error)   
+    }
+}
